fa70d3a05635746bf514433ec63bbd52ed25fb53
Mariama <mariamabintezakaria@gmail.com>
2018-05-30 04:42:21 +0000

    classwork4

--- script.js
+++ script.js
@@ -11,5 +11,27 @@ textBtn.addEventListener('click', function(){
     paragraph.innerText="Hello World"
     task2.appendChild(paragraph)
 })
+//Task 2b
+function changeBGcolor(event){
+    var body=document.getElementsByTagName('body')[0]
+    if(event.target.innerText=="Red"){
+        body.style.backgroundColor='red';
+    }
+    if(event.target.innerText=="Green"){
+        body.style.backgroundColor='green';
+    }
+    if(event.target.innerText=="Purple"){
+        body.style.backgroundColor='purple';
+    }
+}
+
+//Task 2c
+function redirectToGoogle(){
+    window.location.assign('http://google.com')
+}
+function redirectToGithub(){
+    window.location.assign('http://github.com')
+}
+
 
 /* Task 4 using jQuery */
